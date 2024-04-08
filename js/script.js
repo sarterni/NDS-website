f1fd83e28e750ac13d186fd7757489fa7a0f9b3a
sarterni <sarter.nicolas@gmail.com>
2024-04-08 14:13:05 +0200

    navbarr

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-document.querySelector("nav-Togle-btn").onclick = toggleMenu;
+document.querySelector('nav-Togle-btn').onclick = toggleNav;
 
 
 
-function toggleMenu() {
-
-
-    
+function toggleNav() {
+    let nav = document.querySelector('.nav-content');
+    if (nav.style.display === "none") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
 }
