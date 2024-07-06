675ab2296a3da94a8ed15d016cfb07071c0156a9
sarterni <sarter.nicolas@gmail.com>
2024-07-06 10:05:27 +0200

    cache busting ok

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
         integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/HomePage.css" />
-    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" href="css/HomePage.css?v=<?php echo filemtime('css/HomePage.css'); ?>">        <link rel="icon" href="favicon.ico" type="image/x-icon" />
     <title>Nicodigitalsstudo</title>
 </head>
 
